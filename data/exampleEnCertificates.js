5bed082273966ff347375beac839bf5c7d95adf4
Clemens Brunner <clemens.brunner@fh-salzburg.ac.at>
2019-10-26 14:59:35 +0200

    add translation

---
+++ data/exampleEnCertificates.js
@@ -0,0 +1,40 @@
+export default {
+  web: {
+    verify: {
+      placeYourFile: 'Place your file here...',
+      verifyBtnHeader: 'Verify a certificate',
+      verifyBtnText: 'Choose a certificate...',
+      documentRegisteredHeader: 'Certificate registered',
+      registeredAt: 'Registration Time',
+      revokedAt: 'Revocation Time',
+      documentNotRegistered: 'Certificate is not registered',
+      back: 'Back',
+      name: 'Name of the certificate',
+      tryAnother: 'Choose another certificate...',
+      networkError: 'Network error',
+      networkErrorText: 'sproof client is not available, please try again later...',
+      validDocument : 'Certificate is valid',
+      revokedDocument : 'Certificate was revoked',
+      revokedProfile : 'Issuer of certificate is revoked',
+      details: 'Details',
+      timestamp: 'Timestamp',
+      documentHash: 'Document Hash',
+      documentId: 'Document ID',
+      locationHash: 'Document Link',
+      issuerId: 'Issuer ID',
+      ipfsHash: 'IPFS Hash',
+      blocknumber: 'Blocknumber',
+      txHash: 'TransactionHash',
+      chain: 'Chain',
+      issuedBy: 'Issued by',
+      multipleHeader: 'This certificate is registered by several issuers',
+      status: 'Status of document',
+      registered: 'Certificate is registered',
+      notRegistered : 'Certificate is not registered',
+      registeredText_1: "Your certificate, with the name \'%{name}\' is registered in the sproof network by %{count} profile.",
+      registeredText: 'Your certificate, with the name \'%{name}\' is registered in the sproof network by %{count} profiles.',
+      notRegisteredText : 'Your certificate, with the name \'%{name}\' is not registered in the sproof network. It could be that your certificate is in pending mode. Try it again later.',
+      revokedBy : "Revoked by"
+    }
+  }
+}
